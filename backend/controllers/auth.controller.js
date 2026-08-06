58508409a44288d8ba2d72cd8762e24446e859ab
ibrahimkanan <ibrahimkanan20@gmail.com>
2026-08-06 22:51:00 +0300

    feat : setup user model and signup route

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,4 +1,23 @@
-export const signup = async (req, res) => {};
+import User from "../models/user.model.js";
+
+export const signup = async (req, res) => {
+    const { name, email, password } = req.body;
+
+    try {
+        const userExists = await User.findOne({ email });
+
+        if (userExists) {
+            return res.status(400).json({ message: "User already exists" });
+        }
+
+        const user = await User.create({ name, email, password });
+
+        res.status(201).json({ message: "User created successfully", user });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: "Internal server error" });
+    }
+};
 
 export const login = async (req, res) => {};
 
--- backend/server.js
+++ backend/server.js
@@ -7,8 +7,9 @@ dotenv.config();
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+app.use(express.json());
 
-app.use("api/auth", authRoutes);
+app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
